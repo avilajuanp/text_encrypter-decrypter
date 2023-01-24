ec486a7914ec1bbec57a439791534b9a0d745976
Juan Pablo Avila <avilajuanp@gmail.com>
2023-01-24 02:39:19 -0300

    función desencriptar agregada y aplicada al botón.

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ La letra "u" es convertida para "ufat"
 function btnEncrypt(){
     const encryptedText = encrypt(textArea.value); //capturo el texto del input y lo encripto
     outputArea.value = encryptedText; //muestro el texto encriptado en el output
+    textArea.value = "";
+    // outputArea.style.backgroundImage = "none";
+};
+
+function btnDecrypt(){
+    const decryptedText = decrypt(textArea.value);
+    outputArea.value = decryptedText;
+    textArea.value = "";
 };
 
 function encrypt(inputText){
@@ -30,6 +38,27 @@ function encrypt(inputText){
     for (let i = 0; i < encryptCode.length; i++) {
         if (outputText.includes(encryptCode[i][0])) {
             outputText = outputText.replaceAll(encryptCode[i][0], encryptCode[i][1])
+        }
+        
+    };
+
+    return outputText;
+};
+
+function decrypt(inputText){
+    let encryptCode = [
+        ["e", "enter"],
+        ["i", "imes"],
+        ["a", "ai"],
+        ["o", "ober"],
+        ["u", "ufat"],    
+    ];
+    
+    outputText = inputText.toLowerCase(); // lowercase no es necesario xq ya lo fuerzo en CSS
+
+    for (let i = 0; i < encryptCode.length; i++) {
+        if (outputText.includes(encryptCode[i][1])) {
+            outputText = outputText.replaceAll(encryptCode[i][1], encryptCode[i][0])
         }
         
     };
